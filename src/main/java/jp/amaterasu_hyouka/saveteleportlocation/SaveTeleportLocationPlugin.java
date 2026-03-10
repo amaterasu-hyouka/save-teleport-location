ce23b9573ec9d9cd7b36ff7aa222d35f41cf5c61
amaterasu-hyouka <amaterasu.hyouka1218@gmail.com>
2026-03-11 06:20:03 +0900

    feat: プロジェクトのセットアップ
    依存関係の追加
    - Paper API
    - SQLite JDBC
    - MyBatis

---
+++ src/main/java/jp/amaterasu_hyouka/saveteleportlocation/SaveTeleportLocationPlugin.java
@@ -0,0 +1,16 @@
+package jp.amaterasu_hyouka.saveteleportlocation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SaveTeleportLocationPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
