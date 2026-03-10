e61a418b21d99c52e496604f3fa74bf0bddb0bee
amaterasu-hyouka <amaterasu.hyouka1218@gmail.com>
2026-03-11 06:24:39 +0900

    feat: 起動時のログを追加

--- src/main/java/jp/amaterasu_hyouka/saveteleportlocation/SaveTeleportLocationPlugin.java
+++ src/main/java/jp/amaterasu_hyouka/saveteleportlocation/SaveTeleportLocationPlugin.java
@@ -1,16 +1,25 @@
 package jp.amaterasu_hyouka.saveteleportlocation;
 
+import jp.amaterasu_hyouka.saveteleportlocation.utils.Log;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SaveTeleportLocationPlugin extends JavaPlugin {
 
+    private static SaveTeleportLocationPlugin instance;
+    public static JavaPlugin getInstance(){
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        Log.info("SaveTeleportLocationを起動中...");
+        Log.info("SaveTeleportLocationを起動しました");
     }
 
     @Override
     public void onDisable() {
-
+        Log.info("SaveTeleportLocationを停止中...");
+        Log.info("SaveTeleportLocationを停止しました");
     }
 }
---
+++ src/main/java/jp/amaterasu_hyouka/saveteleportlocation/utils/Log.java
@@ -0,0 +1,39 @@
+package jp.amaterasu_hyouka.saveteleportlocation.utils;
+
+import jp.amaterasu_hyouka.saveteleportlocation.SaveTeleportLocationPlugin;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Log {
+
+    private static final String PREFIX = "> ";
+    private static final Logger LOGGER = SaveTeleportLocationPlugin.getInstance().getLogger();
+
+    private Log() {}
+
+    public static void info(String msg) {
+        LOGGER.log(Level.INFO, PREFIX + msg);
+    }
+
+    public static void warn(String msg) {
+        LOGGER.log(Level.WARNING, PREFIX + msg);
+    }
+
+    public static void error(String msg) {
+        LOGGER.log(Level.SEVERE, PREFIX + msg);
+    }
+
+    public static void error(Throwable t) {
+        LOGGER.log(Level.SEVERE, PREFIX + "Unexpected error", t);
+    }
+
+    public static void error(String msg, Throwable t) {
+        LOGGER.log(Level.SEVERE, PREFIX + msg, t);
+    }
+
+    /** デバッグログ（必要な時だけ使う） */
+    public static void debug(String msg) {
+        LOGGER.log(Level.INFO, PREFIX + "[DEBUG] " + msg);
+    }
+}
